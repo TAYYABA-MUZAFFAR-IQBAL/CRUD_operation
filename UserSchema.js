ebb84bb51deb7bb65f06ff179083be085fe6dc26
TAYYABA <tayyaba.tahira98@gmail.com>
2022-06-10 12:30:08 +0500

    AuthLogin

--- UserSchema.js
+++ UserSchema.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema({
     required: true
   },
  
+ 
 });
 
 const user = mongoose.model("User", userSchema);
--- work.js
+++ work.js
@@ -4,16 +4,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userSch = require("./UserSchema");
 const RoleSch = require("./RoleSchema");
+const jwt = require("jsonwebtoken");
+// const expressjwt = require("express-jwt");
+const bcrypt = require("bcrypt");
+// const user = require("./UserSchema");
+require("cookie-parser");
 
 const app = express();
 
 app.use(express.json());
 
-
-
-
 //connectivity with mongoDB
-
 
 mongoose
   .connect("mongodb+srv://tayyaba:tayyaba@cluster0.kql16.mongodb.net/test", {
@@ -24,57 +25,67 @@ mongoose
     console.log("MongoDb connected....");
   });
 //users  CRUD....
-//Add User
+//Add User or sigup
 
-app.post("/UserDB", (req, res) => {
-  console.log(req.body);
-
+app.post("/UserDB/Sigup", async (req, res) => {
+ 
   const userWork = new userSch({
     name: req.body.name,
+    role: req.body.role,
     email: req.body.email,
     password: req.body.password,
-    role: req.body.role,
   });
+  // const oldUser = await user.findOne({ email });
+  // if (oldUser) {
+  //   return res.status(409).send("User Already exsists plz Login");
+  // }
   userWork.save((err, user) => {
     if (err) {
       res.json({ errmsg: err });
-      return;ho
+      return;
+      ho;
     } else {
       res.json({ msg: user });
+     
       console.log("User Added Successfully to DB");
     }
   });
-});
+ 
+ });
 
 //Get all User
 app.get("/UserDB", (req, res) => {
-  userSch.find({}, (err, users) => {
-    if (err) {
-      console.log(err);
-      res.status(500).json({ errmsg: err });
-      return;
-    } else {
-      console.log("users", users);
+  userSch
+    .find({}, (err, users) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json({ errmsg: err });
+        return;
+      } else {
+        console.log("users", users);
 
-      res.status(200).json({ msg: users });
-      console.log("All user data is extracted");
-    }
-  }).populate('role');
+        res.status(200).json({ msg: users });
+        console.log("All user data is extracted");
+      }
+    })
+    .populate("role");
 });
 
 //get User by id
 app.get("/UserDB/:id", (req, res) => {
-  userSch.findById({ _id: req.params.id }, (err, users) => {
-    if (err) {
-      console.log(err);
-      res.status(500).json({ errmsg: err });
-      return;
-    } else {
-      res.status(200).json({ msg: users });
-      console.log("users", users);
-      console.log("require useer id is extracted");
-    }
-  }).populate('role');
+  userSch
+    .findById({ _id: req.params.id }, (err, users) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json({ errmsg: err });
+        return;
+      } else {
+        res.status(200).json({ msg: users });
+        console.log("users", users);
+        console.log("require useer id is extracted");
+      }
+    })
+    .populate("role");
 });
 
 //update user
@@ -86,9 +97,9 @@ app.put("/UserDB/:id", (req, res) => {
       return;
     } else {
       (users.name = req.body.name),
-        (users.email = req.body.email),
-        (users.password = req.body.password),
-        (users.role = req.body.role);
+       (users.email = req.body.email);
+       (users.password = req.body.password);
+       (users.role = req.body.role);
       users.save((err, users) => {
         if (err) {
           res.status(500).json({ errmsg: err });
@@ -208,6 +219,37 @@ app.listen(process.env.PORT || 4000, () => {
   console.log("listening on Port 4000");
 });
 
-app.get('/', (req, res) => {
-  res.send('App is working!')
-})
+app.get("/", (req, res) => {
+  res.send("App is working!");
+});
+
+
+//LOGIN Authentication
+
+app.post("/Login", async(req, res) => {
+
+try {
+  const {email, password}= req.body
+  const user=await userSch.findOne({email})
+  if(!user)
+  { res.json({status:'error',error:'Invalid username/password'})}
+  const passwordCompare= await bcrypt.compare(password, user.password);
+   if(!passwordCompare){
+     const token=jwt.sign(
+       {
+         id:user._id,
+         email:user.email
+       },
+       'secret123',{
+         expiresIn:120
+       }
+     )
+     return res.json({user:user,token: token})
+     
+   }else{
+     return res.json({status:'error',error: 'check the password again '})
+   }
+} catch (error) {
+  console.log(error);
+}
+});
